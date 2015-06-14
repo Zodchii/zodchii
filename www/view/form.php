82f389fd3e59860bc5259624ab6a1bda92f0ff28
Zodchii <maxim.rozenberg@gmail.com>
2015-06-14 12:23:30 +0300

    xxxxxxxxxx

---
+++ www/view/form.php
@@ -0,0 +1,7 @@
+<form action="/add.php" method="post" enctype="multipart/form-data">
+    <label for="title">Lable</label>
+    <input type="text" id="title" name="title">
+    <label for="news">News</label>
+    <input type="file" id="news" name="news">
+    <input type="submit" value="submit">
+</form>
